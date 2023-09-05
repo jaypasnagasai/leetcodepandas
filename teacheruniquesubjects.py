8c3786a07ae373ce569d1dd335981e6968dd7f11
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-04 19:17:40 -0700

    Create teacheruniquesubjects.py

---
+++ teacheruniquesubjects.py
@@ -0,0 +1,10 @@
+# 2356. Number of Unique Subjects Taught by Each Teacher
+
+import pandas as pd
+
+def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
+    # Group by teacher_id and count the number of unique subject_ids
+    result = teacher.groupby('teacher_id')['subject_id'].nunique().reset_index()
+    result.rename(columns={'subject_id': 'cnt'}, inplace=True)
+    
+    return result
