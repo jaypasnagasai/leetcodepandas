98ec0da242865895825bdb6a033062c158edae5a
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-04 19:00:05 -0700

    Create patientswithcondition.py

---
+++ patientswithcondition.py
@@ -0,0 +1,8 @@
+# 1527. Patients With a Condition
+
+import pandas as pd
+
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    return patients[
+        patients['conditions'].str.contains(r'(^DIAB1)|( DIAB1)')
+    ]
