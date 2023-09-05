3fb138a9b00c8b84c737cdbbc69b82ae8c7ae30e
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-04 18:58:18 -0700

    Create userswithvalidemialids.py

---
+++ userswithvalidemialids.py
@@ -0,0 +1,8 @@
+# 1517. Find Users With Valid E-Mails
+
+import pandas as pd
+
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    return users[
+        users['mail'].str.match(r'^[a-zA-Z][a-zA-Z\d_.-]*@leetcode\.com')
+    ]
